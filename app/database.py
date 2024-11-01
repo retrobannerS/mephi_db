ce8a8532238db8993fa013fa105893ce3eb92978
retrobanner <live@goml.ru>
2024-11-01 18:52:48 +0300

    refactor custom types and database mappings: rename 'adress' to 'str_100' and update type_annotation_map accordingly

--- app/database.py
+++ app/database.py
@@ -1,11 +1,11 @@
 import os
 from sqlalchemy import create_engine, String
 from sqlalchemy.orm import session, sessionmaker, DeclarativeBase
-from custom_types import phone_number, adress, title, name
+from custom_types import phone_number, str_100
 
 engine = create_engine(url=os.getenv("DSN"), echo=True)
 
 session = sessionmaker(engine)
 
 class Base(DeclarativeBase):
-    type_annotation_map = {phone_number: String(10), name: String(100), title: String(100), adress: String(100)}
+    type_annotation_map = {phone_number: String(10), str_100: String(100)}
