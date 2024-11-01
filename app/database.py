70c2cfde0e4a0262fda880cb5fd3242eae5ddb6e
retrobanner <live@goml.ru>
2024-11-01 15:07:03 +0300

    Add sessionmaker to database.py for session management

--- app/database.py
+++ app/database.py
@@ -1,4 +1,7 @@
 import os
 from sqlalchemy import create_engine
+from sqlalchemy.orm import session, sessionmaker
 
 engine = create_engine(os.getenv("DSN"), echo=False)
+
+session = sessionmaker(engine)
