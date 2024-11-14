67de092a430042e9dfaae44aca85ec95be7878fa
retrobanner <live@goml.ru>
2024-11-14 12:45:14 +0300

    feat: add payment methods seeder and integrate it into the seeding process

--- app/seed.py
+++ app/seed.py
@@ -2,4 +2,6 @@ from seeders import *
 
 
 def seed_all():
+    seed_payment_methods()
     seed_users(10)
+    
---
+++ app/seeders/payment_methods_seeder.py
@@ -0,0 +1,13 @@
+from database import session
+import models
+
+def seed_payment_methods():
+    payment_methods = [
+        models.Payment_Methods(title="Карта"),
+        models.Payment_Methods(title="Карта курьеру"),
+        models.Payment_Methods(title="Наличные курьеру"),
+        models.Payment_Methods(title="Яндекс.Сплит")
+    ]
+    
+    session.bulk_save_objects(payment_methods)
+    session.commit()
