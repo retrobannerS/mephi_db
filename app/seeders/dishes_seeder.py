e6f270e93d924a1913983a37bbe25bf46a12a584
retrobanner <live@goml.ru>
2024-11-14 22:23:05 +0300

    feat: add dishes seeder and corresponding CSV file for dish data

---
+++ app/seeders/dishes_seeder.py
@@ -0,0 +1,20 @@
+from database import session
+from models import Dishes
+import csv
+import os
+
+
+def seed_dishes():
+    file_path = os.path.join(os.path.dirname(__file__), "dishes.csv")
+    with open(file_path, "r") as file:
+        reader = csv.DictReader(file)
+        dishes = []
+
+        for row in reader:
+            dish = Dishes(
+                title=row["title"], type=row["type"], weight=float(row["weight"]), colorfulness=float(row["colorfulness"]),
+            )
+            dishes.append(dish)
+
+    session.bulk_save_objects(dishes)
+    session.commit()
