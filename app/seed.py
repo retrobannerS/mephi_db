88ad8df490491392ffb8daaddcd973d7ec333724
retrobanner <live@goml.ru>
2024-11-16 15:52:52 +0300

    Change number of rows in seeding tables

--- app/seed.py
+++ app/seed.py
@@ -3,18 +3,18 @@ from seeders import *
 
 def seed_all():
     seed_payment_methods()
-    seed_users(10)
+    seed_users(15000)
     seed_payment_infos()
 
-    seed_couriers(5)
+    seed_couriers(1000)
     seed_menus()
-    seed_orders(50, 30)
+    seed_orders(50000, 100)
 
     seed_preference_categories()
     seed_preferences()
     seed_preferences_users()
 
-    seed_suppliers(10)
+    seed_suppliers(500)
     seed_ingredients()
     seed_ingredients_suppliers()
     seed_dishes()
