40a5dc1cd1e23ca50c9ac43ae2a74da56b58c328
retrobanner <live@goml.ru>
2024-11-27 13:57:04 +0300

    feat: update payment_infos seeder

--- app/seeders/payment_infos_seeder.py
+++ app/seeders/payment_infos_seeder.py
@@ -12,9 +12,11 @@ def seed_payment_infos():
     payment_infos = []
     for user in users:
         for payment_method in payment_methods:
+            if random.random() < 0.5:
+                continue
             payment_info = models.Payment_infos(
                 user_id=user.id,
-                payment_method_id=payment_method.id if random.random() < 0.5 else None,
+                payment_method_id=payment_method.id,
                 requisites=fake.credit_card_number() if payment_method.require_requisites else None
             )
             payment_infos.append(payment_info)
